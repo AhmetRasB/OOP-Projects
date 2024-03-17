a3dbc054b8f7ee0ef9995276b093e7837dc37da3
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-03-17 17:41:00 +0300

    Add project files.

---
+++ OOP-Projects/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Projects
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Insan insan1 = new Insan();
+            insan1.AdSoyad = "Duygu Balaban";
+            insan1.CanliMi = true;
+            Insan insan2 = new Insan();
+            insan2.AdSoyad = "Ahmet Balaban";
+            insan2.CanliMi = true;
+
+
+        }
+    }
+}
---
+++ OOP-Projects/Insan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Projects
+{
+    internal class Insan
+    {
+        public string AdSoyad { get; set; }
+
+        public bool CanliMi { get; set; }
+        public Insan()
+        {
+            CanliMi = true;
+        }
+        public Insan(string AdSoyAd)
+        {
+            AdSoyad = AdSoyAd;
+            CanliMi = true;
+        }
+        public static string Selamver()
+        {
+            return "Merhaba";
+             
+        }
+        public string Adinisoyle()
+        {
+            return "Benim adim " + AdSoyad;
+        }
+    }
+}
