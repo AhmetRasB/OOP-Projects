beed4212afc55175a55e082638da0be07c4fc48b
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-03-17 18:04:43 +0300

    .

--- OOP-Projects/Form1.cs
+++ OOP-Projects/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -25,8 +26,32 @@ namespace OOP_Projects
             Insan insan2 = new Insan();
             insan2.AdSoyad = "Ahmet Balaban";
             insan2.CanliMi = true;
+            Insan insan3 = new Insan("Mesut Gül");
+            Insan insan4 = new Insan("Caner Kırma");
+            Insan insan5 = new Insan()
+            {
+                AdSoyad = "Erkan Zengin",
+                CanliMi = true
+            };
+            MessageBox.Show(Insan.Selamver());
+            MessageBox.Show(insan2.Adinisoyle());
+           
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TCVatandas vat1 = new TCVatandas();
+            vat1.DogumTarihi = Convert.ToDateTime("1999-05-05");
+            MessageBox.Show(vat1.Vizeal());
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ABDVatandas abd1 = new ABDVatandas();
+            abd1.AdSoyad = "Michael Jackson";
+            abd1.Dil = "English";
+            abd1.DogumTarihi = Convert.ToDateTime("1975-12-12");
+            MessageBox.Show(abd1.Yas.ToString());
         }
     }
 }
---
+++ OOP-Projects/TCVatandas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Projects
+{
+    internal class TCVatandas
+    {
+        public long TCKimlikNo { get; set; }
+        public string AdSoyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public DateTime SistemeGirisTarihi { get; set; }
+
+        public TCVatandas()
+        {
+            SistemeGirisTarihi = DateTime.Now;
+            
+        }
+        public string Vizeal() 
+        {
+            int yas = YasHesapla(DogumTarihi);
+            if (yas >= 18)
+                return "Başvurabilirsiniz";
+            else
+                return "Veli izni gerekmektedir";
+        }
+
+        private int YasHesapla(DateTime dogumTarihi)
+        {
+            return DateTime.Today.Year-dogumTarihi.Year;
+
+        }
+
+    }
+}
